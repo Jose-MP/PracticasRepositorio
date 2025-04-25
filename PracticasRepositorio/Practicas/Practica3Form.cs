976c3f94a4c80612cd443131fb147e0e60f75031
Jose Martinez <jr010605@gmail.com>
2025-04-24 17:50:31 -0700

    asd

--- PracticasRepositorio/Practicas/Practica3Form.cs
+++ PracticasRepositorio/Practicas/Practica3Form.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClassLibrary1;
-
+//a
 namespace PracticasRepositorio.Practicas
 {
     public partial class Practica3Form : Form
